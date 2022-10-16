83457340f78119bb783fa4d736dbd0f5f77a8e35
vExcess <41875927+vExcess@users.noreply.github.com>
2022-10-15 23:39:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
 var http = require("http");
-// var fetch = require("./node_modules/node-fetch/lib/index");
+var fetch = require("./dependencies/node-fetch");
 
 var httpServer = http.createServer(async function(req, res) {
-    console.log(require("./dependencies/qwe.js"));
-    
     try {
-//         var query = req.url.slice(req.url.indexOf("?") + 1);
-//         var len = query.length;
+        var query = req.url.slice(req.url.indexOf("?") + 1);
+        var len = query.length;
 
-//         var queryData = {};
+        var queryData = {};
         
-//         var i = 0;
-//         while (i < len) {
-//             if (query.slice(i, i + 4) === "=%22") {
-//                 let key, value;
-//                 key = value = undefined;
+        var i = 0;
+        while (i < len) {
+            if (query.slice(i, i + 4) === "=%22") {
+                let key, value;
+                key = value = undefined;
                 
-//                 let j = i - 1;
-//                 while (query.charAt(j) !== "?" && query.charAt(j) !== "&" && j >= 0) {
-//                     j--;
-//                 }
-//                 key = query.slice(j + 1, i);
+                let j = i - 1;
+                while (query.charAt(j) !== "?" && query.charAt(j) !== "&" && j >= 0) {
+                    j--;
+                }
+                key = query.slice(j + 1, i);
 
-//                 j = i + 2;
-//                 while (query.slice(j, j + 3) !== "%22" && j < len) {
-//                     j++;
-//                 }
-//                 value = decodeURIComponent(query.slice(i + 4, j));
-//                 i = j;
+                j = i + 2;
+                while (query.slice(j, j + 3) !== "%22" && j < len) {
+                    j++;
+                }
+                value = decodeURIComponent(query.slice(i + 4, j));
+                i = j;
 
-//                 queryData[key] = value;
-//             }
+                queryData[key] = value;
+            }
 
-//             i++;
-//         }
+            i++;
+        }
 
-//         console.log(queryData);
+        console.log(queryData);
 
-//         if (queryData.url && queryData.user) {
-//             let proxRes = await fetch(queryData.url);
-//             let arrBuff = await proxRes.arrayBuffer();
-//             let resBytes = new Uint8Array(arrBuff);
+        if (queryData.url && queryData.user) {
+            let proxRes = await fetch(queryData.url);
+            let arrBuff = await proxRes.arrayBuffer();
+            let resBytes = new Uint8Array(arrBuff);
 
-//             res.setHeader("Access-Control-Allow-Origin", "*");
-//             res.write(resBytes);
-//         }
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.write(resBytes);
+        }
 
         res.end();
     } catch (err) {
