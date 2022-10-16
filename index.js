141b87a8d7d2616fa1a7b3361e7e2a64bd62a3fa
vExcess <41875927+vExcess@users.noreply.github.com>
2022-10-15 22:58:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-var http = require('http');
+var http = require("http");
+var fetch = require("node-fetch");
 
-var httpServer = http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
+var httpServer = http.createServer(async function(req, res) {
+    try {
+        var query = req.url.slice(req.url.indexOf("?") + 1);
+        var len = query.length;
+
+        var queryData = {};
+        
+        var i = 0;
+        while (i < len) {
+            if (query.slice(i, i + 4) === "=%22") {
+                let key, value;
+                key = value = undefined;
+                
+                let j = i - 1;
+                while (query.charAt(j) !== "?" && query.charAt(j) !== "&" && j >= 0) {
+                    j--;
+                }
+                key = query.slice(j + 1, i);
+
+                j = i + 2;
+                while (query.slice(j, j + 3) !== "%22" && j < len) {
+                    j++;
+                }
+                value = decodeURIComponent(query.slice(i + 4, j));
+                i = j;
+
+                queryData[key] = value;
+            }
+
+            i++;
+        }
+
+        console.log(queryData);
+
+        if (queryData.url && queryData.user) {
+            let proxRes = await fetch(url);
+            let arrBuff = await proxRes.arrayBuffer();
+            let resBytes = new Uint8Array(arrBuff);
+
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.write(resBytes);
+        }
+
+        res.end();
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-httpServer.listen(3000);
+httpServer.listen(3000, () => {
+    console.log("Server Online!");
+});
